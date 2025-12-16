3bd73ce4affc31fa91ad700c229aa2977639de05
한재호 <wogh009@gmail.com>
2025-12-16 17:49:41 +0900

    Initial commit

---
+++ src/main/java/kr/java/xmastree/XmasTreeApplication.java
@@ -0,0 +1,13 @@
+package kr.java.xmastree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmasTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmasTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/java/xmastree/XmasTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.java.xmastree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmasTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
